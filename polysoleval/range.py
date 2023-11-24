ede1af4ecad8bdbe9460015465447c5200f06616
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-11-24 17:42:01 -0500

    update Range

--- polysoleval/range.py
+++ polysoleval/range.py
@@ -9,8 +9,8 @@ class Range:
     min_value: float
     max_value: float
     log_scale: bool = False
-    scale_min: float = field(init=False)
-    diff: float = field(init=False)
+    _scale_min: float = field(init=False, repr=False)
+    _diff: float = field(init=False, repr=False)
 
     def __post_init__(self):
         if self.min_value >= self.max_value:
@@ -20,20 +20,20 @@ class Range:
                 "min_value and max_value must be positive if log_scale is True"
             )
         if self.log_scale:
-            self.scale_min = log10(self.min_value)
-            self.diff = log10(self.max_value / self.min_value)
+            self._scale_min = log10(self.min_value)
+            self._diff = log10(self.max_value / self.min_value)
         else:
-            self.scale_min = self.min_value
-            self.diff = self.max_value - self.min_value
+            self._scale_min = self.min_value
+            self._diff = self.max_value - self.min_value
 
     def normalize(self, tensor: torch.Tensor) -> None:
         if self.log_scale:
             tensor.log10_()
-        tensor -= self.scale_min
-        tensor /= self.diff
+        tensor -= self._scale_min
+        tensor /= self._diff
 
     def unnormalize(self, tensor: torch.Tensor) -> None:
-        tensor *= self.diff
-        tensor += self.scale_min
+        tensor *= self._diff
+        tensor += self._scale_min
         if self.log_scale:
             torch.pow(10.0, tensor, out=tensor)
