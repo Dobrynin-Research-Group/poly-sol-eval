b825f32cb81cacb416a6a27cbcfe626c5dcf23c6
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2023-12-04 15:08:33 -0500

    add NU and AV to globals

--- polysoleval/globals.py
+++ polysoleval/globals.py
@@ -6,10 +6,31 @@ from polysoleval.models import NeuralNetPair, NeuralNetType
 from polysoleval.datafile import DatafileHandler
 
 
+__all__ = [
+    "NetRangePairNames",
+    "NU",
+    "NU3_1",
+    "NU2_1",
+    "REDUCED_AVOGADRO",
+    "NEURALNETPATH",
+    "RANGEPATH",
+    "TMPPATH",
+    "HANDLER",
+    "NEURALNET_TYPES",
+    "NEURALNET_PAIRS",
+]
+
+
 class NetRangePairNames(NamedTuple):
     net_name: str
     range_name: str
 
+
+NU = 0.588  # good solvent exponent
+NU3_1 = 3 * NU - 1
+NU2_1 = 2 * NU - 1
+
+REDUCED_AVOGADRO = 0.60221408  # Avogadro number divieded by 10^24
 
 NEURALNETPATH = Path(environ["modelpath"])
 RANGEPATH = Path(environ["rangepath"])
